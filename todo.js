64d7c40f2239463916c7e18ec7917032db9c3ef4
poojitha4443 <76087467+poojitha4443@users.noreply.github.com>
2023-01-06 18:31:58 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -10,7 +10,7 @@ describe("Todo List Test Suite", () => {
     const oneDay = 60 * 60 * 24 * 1000;
     [
       {
-        title: "Buy milk",
+        title: "Buy water",
         completed: false,
         dueDate: new Date(today.getTime() - 2 * oneDay).toLocaleDateString(
           "en-CA"
